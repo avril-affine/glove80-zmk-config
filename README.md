92780d186f9171929994bcc46ca70a2cb1780a2e
kenny <kkihara1@gmail.com>
2024-03-16 00:02:46 -0700

    update

--- README.md
+++ README.md
@@ -7,12 +7,6 @@ tar xvf zephyr-sdk-0.16.4_macos-aarch64.tar.xz -C ~/.local/zephyr-sdk
 cd ~/.local/zephyr-sdk
 ./setup.sh
 
-# zephyr
-pip install west
-west init ~/zephyrproject
-west update
-west zephyr-export
-
-# this app
+cd $PROJECT_ROOT
 ./build.sh
 ```
--- build.sh
+++ build.sh
@@ -1,16 +1,45 @@
 #!/usr/bin/env bash
 
-set -e -x
+
+on_error() {
+    set +x
+    while [ $(dirs | wc -w) -gt 1 ]; do
+        popd
+    done
+    exit 1
+}
+
+trap "on_error" ERR
+set -x
+
+if ! command -v west; then
+    echo 'west is not install. try "pip install west"'
+    exit 1
+fi
+
+if [ ! -d "zmk/zephyr" ]; then
+    pushd zmk
+    west init -l app/
+    popd
+fi
+
+if [ ! -d "zmk/modules" ]; then
+    pushd zmk
+    west update
+    west zephyr-export
+    popd
+fi
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 APP_DIR="${SCRIPT_DIR}/zmk/app"
 CONFIG_DIR="${SCRIPT_DIR}/config"
 OUT="${SCRIPT_DIR}/build/$(date -Iseconds).uf2"
 
-cd $APP_DIR
+pushd $APP_DIR
+echo $PWD
 west build -p -b glove80_lh -d build/left -- -DZMK_CONFIG=$CONFIG_DIR
 west build -p -b glove80_rh -d build/right -- -DZMK_CONFIG=$CONFIG_DIR
 mkdir -p "$(dirname $OUT)"
 cat build/left/zephyr/zmk.uf2 build/right/zephyr/zmk.uf2 > $OUT
 ln -sf $OUT "${SCRIPT_DIR}/latest.uf2"
-cd -
+popd
